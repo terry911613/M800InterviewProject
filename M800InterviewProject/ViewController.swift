caad0963c0193321d0cff0217ac07916196d6348
TaiYiLee <terry911613@gmail.com>
2021-05-14 15:26:02 +0800

    Initial Commit

---
+++ M800InterviewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M800InterviewProject
+//
+//  Created by 李泰儀 on 2021/5/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
